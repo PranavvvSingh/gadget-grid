ad19308211487926d9952db981ffd72c020708ae
PranavvvSingh <pranavvalt@gmail.com>
2023-12-24 12:07:06 +0530

    fixed-cart

--- frontend/src/components/Cart.tsx
+++ frontend/src/components/Cart.tsx
@@ -1,44 +1,30 @@
 import { Rating } from "react-simple-star-rating";
-import { phoneType } from "../types/types";
+import { cardType, phoneType } from "../types/types";
 import { useMobileCartContext } from "@/context/ExportContext";
 import { useEffect, useState } from "react";
 import { MdDeleteOutline } from "react-icons/md";
 import { CiShoppingCart } from "react-icons/ci";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import Loader from "./Loader";
 
 type responseType = {
   data: phoneType;
 };
 
-type propsType = {
-  id: number;
-  quantity: number;
-};
-
-const CartItem = ({ id, quantity }: propsType) => {
-  const navigate= useNavigate()
+const CartItem = ({ id, name, price, rating, image }: cardType) => {
+  const { cartItems } = useMobileCartContext();
+  const item =cartItems.find((item)=>item.id===id)
+  const quantity= item!.quantity
+  const navigate = useNavigate();
   const { incItemQuantity, decItemQuantity, removeFromCart } =
     useMobileCartContext();
-
-  const [phone, setPhone] = useState<phoneType>({} as phoneType);
-  useEffect(() => {
-    const fetchData = async () => {
-      const { data }: responseType = await axios.get(
-        "https://gadgetsapi-xkx9.onrender.com/products" + "/" + id
-      );
-      setPhone(data);
-    };
-    fetchData();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  console.log("render")
 
   return (
     <div className="grid grid-cols-3 md:grid-cols-5 md:gap-5 items-center mt-3">
       <div className="flex col-span-2 md:col-span-3">
         <img
-          src={phone?.image}
+          src={image}
           alt=""
           className="h-[120px] md:h-[150px]  object-contain cursor-pointer"
           onClick={() => navigate("/product/" + id)}
@@ -49,12 +35,12 @@ const CartItem = ({ id, quantity }: propsType) => {
               className="text-xl md:text-2xl cursor-pointer"
               onClick={() => navigate("/product/" + id)}
             >
-              {phone?.name}
+              {name}
             </h1>
             <Rating
               allowFraction
               size={20}
-              initialValue={phone?.rating}
+              initialValue={rating}
               readonly
               style={{
                 direction: "ltr",
@@ -66,7 +52,7 @@ const CartItem = ({ id, quantity }: propsType) => {
             />
           </div>
           <p className="text-base md:text-lg font-semibold text-neutral-600">
-            ${phone?.price}
+            ${price}
           </p>
         </div>
       </div>
@@ -91,31 +77,41 @@ const CartItem = ({ id, quantity }: propsType) => {
           className="hidden md:block text-3xl text-red-600 cursor-pointer"
         />
       </div>
-      <p className="text-end col-span-3 md:text-center md:col-span-1">Subtotal= ${(quantity * phone?.price).toFixed(2)}</p>
+      <p className="text-end col-span-3 md:text-center md:col-span-1">
+        Subtotal= ${(quantity * price).toFixed(2)}
+      </p>
     </div>
   );
 };
 
-async function fetchPhonePrice(id: number){
+async function fetchPhone(id: number) {
   const { data }: responseType = await axios.get(
     "https://gadgetsapi-xkx9.onrender.com/products" + "/" + id
   );
-  return data.price
+  return data;
 }
 
 const Cart = () => {
   const { cartItems } = useMobileCartContext();
-  const [total, setTotal] = useState(0)
-  useEffect(()=>{
-    setTotal(0)
-    async function fetchTotal(){
-      for(const item of cartItems){
-        const data = await fetchPhonePrice(item.id)
-        setTotal(curr=>curr+data*item.quantity)
+  const [loading, setLoading] = useState(true)
+  const [phones, setPhones] = useState<phoneType[]>([]);
+  const [total, setTotal] = useState(0);
+  useEffect(() => {
+    setTotal(0);
+    setPhones([])
+    async function fetchTotal() {
+      setLoading(true)
+      for (const item of cartItems) {
+        const data = await fetchPhone(item.id);
+        setPhones((curr) => [...curr, data]);
+        setTotal((curr) => curr + data.price * item.quantity);
       }
+      setLoading(false)
     }
-    fetchTotal()
-  },[cartItems])
+    fetchTotal();
+  }, [cartItems]);
+  if(loading) return <Loader/>
+  else
   return (
     <div className="flex flex-col w-5/6 mx-auto mb-10">
       <div className="text-xl md:text-3xl flex gap-2 justify-center mt-3 mb-3">
@@ -127,17 +123,23 @@ const Cart = () => {
         <div>TOTAL</div>
       </div>
       <div className="bg-neutral-300 h-[1px] mt-1"></div>
-      {cartItems.map((item) => {
+      {phones.map((phone) => {
         return (
           <CartItem
-            key={crypto.randomUUID()}
-            id={item.id}
-            quantity={item.quantity}
+            key={phone.id}
+            id={phone.id}
+            name={phone.name}
+            price={phone.price}
+            rating={phone.rating}
+            image={phone.image}
+            remainingUnits={phone.remainingUnits}
           />
         );
       })}
       <div className="bg-neutral-300 h-[1px] mt-2"></div>
-      <h1 className="text-end text-lg md:text-2xl mt-4 md:mx-10">Total = ${total.toFixed(2)}</h1>
+      <h1 className="text-end text-lg md:text-2xl mt-4 md:mx-10">
+        Total = ${total.toFixed(2)}
+      </h1>
     </div>
   );
 };
--- frontend/src/components/ui/select.tsx
+++ frontend/src/components/ui/select.tsx
@@ -68,7 +68,8 @@ SelectScrollDownButton.displayName =
 const SelectContent = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof SelectPrimitive.Content>
->(({ className, children, position = "popper", ...props }) => (
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+>(({ className, children, position = "popper", ...props }, _ref) => (
   <SelectPrimitive.Portal>
     <SelectPrimitive.Content
       ref={(ref) =>
